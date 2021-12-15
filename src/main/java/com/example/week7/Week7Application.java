359d8d946b5c313d56dce9fae24e2950b474562b
Gadibia Daro <55360356+dizsart@users.noreply.github.com>
2021-12-15 13:24:39 +0100

    CHANGE METHOD RETURN VALUE

--- src/main/java/com/example/week7/Week7Application.java
+++ src/main/java/com/example/week7/Week7Application.java
@@ -2,7 +2,9 @@ package com.example.week7;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Profile;
 
+@Profile("test")
 @SpringBootApplication
 public class Week7Application {
 
--- src/test/java/com/example/week7/ServiceImpl/EmployeeServiceImplTest.java
+++ src/test/java/com/example/week7/ServiceImpl/EmployeeServiceImplTest.java
@@ -1,34 +1,42 @@
 package com.example.week7.ServiceImpl;
 
 import com.example.week7.Models.Employee;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.quality.Strictness;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @ActiveProfiles("Test")
-@RunWith(SpringJUnit4ClassRunner.class)
-class EmployeeServiceImplTest {
-
+@RunWith(MockitoJUnitRunner.Silent.class)
+public class EmployeeServiceImplTest {
+    @Mock
     private EmployeeServiceImpl employeeService;
+    private final Employee employee = new Employee();
 
-    private Employee employee;
-
-    @Autowired
-    EmployeeServiceImplTest(EmployeeServiceImpl employeeService) {
-        this.employeeService = employeeService;
-    }
 
     @Test
     public void testAddEmployee(){
+
         employee.setId(1l);
         employee.setName("Agbero Oga");
         employee.setDesignation("Streets");
 
         Mockito.when(employeeService.save(employee)).thenReturn(employee);
 
+
+    }
+
+    @Test
+    public void getEmployeeShouldThrowNullPointerException() throws Exception {
+
+        employee.setId(1l);
+        employee.setName("Agbero Oga");
+        employee.setDesignation("Streets");
 
     }
 
