a14a78bf3ceb9d5683edbbae012882d63236ceb5
daro-decagon <gadibiao@decagonhq.com>
2023-08-25 11:35:02 +0100

    added new rest template

--- src/main/java/com/example/week7/Configuration/SwaggerConfig.java
+++ src/main/java/com/example/week7/Configuration/SwaggerConfig.java
@@ -1,5 +1,14 @@
 package com.example.week7.Configuration;
 
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
 public class SwaggerConfig {
+    @Bean
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
 }
